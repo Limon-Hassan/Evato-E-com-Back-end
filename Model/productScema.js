fdbe41ed6bd34ff3cec9ec4e2f6055f38fc6ded3
Mahammud Hassan Limon <156072234+Limon-Hassan@users.noreply.github.com>
2025-09-06 03:56:51 +0600

    Remove env and node_modules from tracking

---
+++ Model/productScema.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+
+let ProductSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'name is required'],
+      trim: true,
+      text: true,
+    },
+    discription: {
+      type: String,
+      require: true,
+      text: true,
+    },
+    photo: [String],
+    sold: {
+      type: Number,
+      default: 0,
+    },
+    reviews: {
+      type: Number,
+      default: 0,
+    },
+    Totoalreviews: {
+      type: Number,
+      default: 0,
+    },
+    category: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'category',
+      },
+    ],
+    stock: {
+      type: Number,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      require: true,
+    },
+    discountPrice: {
+      type: Number,
+    },
+    createdAt: { type: Date, default: Date.now },
+  },
+  {
+    timestamps: true,
+  }
+);
+ProductSchema.index({ name: 'text', description: 'text' });
+module.exports = mongoose.model('Products', ProductSchema);
